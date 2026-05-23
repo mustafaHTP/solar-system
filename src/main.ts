ed90fdae2cc3af314ccb7672ab09aafba02c0a4c
Mustafa Hatipoğlu <56385804+mustafaHTP@users.noreply.github.com>
2026-05-23 17:20:19 +0300

    chore: update code structure for better readability and maintainability

--- src/main.ts
+++ src/main.ts
@@ -3,46 +3,122 @@ import { OrbitControls } from "three/examples/jsm/Addons.js";
 import { buildCamera } from "./camera";
 import { Planet, type PlanetData } from "./planet";
 
-// Init scene elements
-// sun
-const sunData: PlanetData = {
-  name: "sun",
-  color: 0xf2ff03,
-  scale: 5,
-};
-const earthData: PlanetData = {
-  name: "earth",
-  color: 0x0339ff,
-  scale: 1,
-};
-const moonData: PlanetData = {
-  name: "moon",
-  color: 0xbababa,
-  scale: 0.5,
-};
+// Load textures
+const textureLoader = new THREE.TextureLoader();
 
-const sun = new Planet(sunData);
-const earth = new Planet(earthData);
-const moon = new Planet(moonData);
+const sunTexture = await textureLoader.loadAsync(
+  "src/assets/textures/2k_sun.jpg",
+);
+sunTexture.colorSpace = THREE.SRGBColorSpace;
+
+const earthTexture = await textureLoader.loadAsync(
+  "src/assets/textures/2k_earth.jpg",
+);
+earthTexture.colorSpace = THREE.SRGBColorSpace;
+
+const marsTexture = await textureLoader.loadAsync(
+  "src/assets/textures/2k_mars.jpg",
+);
+marsTexture.colorSpace = THREE.SRGBColorSpace;
+
+const mercuryTexture = await textureLoader.loadAsync(
+  "src/assets/textures/2k_mercury.jpg",
+);
+mercuryTexture.colorSpace = THREE.SRGBColorSpace;
+
+const moonTexture = await textureLoader.loadAsync(
+  "src/assets/textures/2k_moon.jpg",
+);
+moonTexture.colorSpace = THREE.SRGBColorSpace;
+
+const venusTexture = await textureLoader.loadAsync(
+  "src/assets/textures/2k_venus.jpg",
+);
+venusTexture.colorSpace = THREE.SRGBColorSpace;
+
+const planetDataArray: PlanetData[] = [
+  {
+    name: "sun",
+    scale: 5,
+    orbitRadius: 0,
+    orbitSpeed: 0,
+    selfRotationSpeed: 5,
+    texture: sunTexture,
+    moons: [],
+  },
+  {
+    name: "earth",
+    scale: 1,
+    orbitRadius: 20,
+    orbitSpeed: 0.5,
+    selfRotationSpeed: 100,
+    texture: earthTexture,
+    moons: [],
+  },
+  {
+    name: "mercury",
+    scale: 0.5,
+    orbitRadius: 10,
+    orbitSpeed: 0.4,
+    selfRotationSpeed: 0.01,
+    texture: mercuryTexture,
+    moons: [],
+  },
+  {
+    name: "venus",
+    scale: 0.8,
+    orbitRadius: 15,
+    orbitSpeed: 0.8,
+    selfRotationSpeed: 0.007,
+    texture: venusTexture,
+    moons: [],
+  },
+  {
+    name: "mars",
+    scale: 0.7,
+    orbitRadius: 25,
+    orbitSpeed: 0.3,
+    selfRotationSpeed: 0.003,
+    texture: marsTexture,
+    moons: [],
+  },
+  {
+    name: "moon",
+    scale: 0.5,
+    orbitRadius: 15,
+    orbitSpeed: 0.5,
+    selfRotationSpeed: 10,
+    texture: moonTexture,
+    moons: [],
+  },
+];
+
+// construct planets
+const planets: Planet[] = planetDataArray.map((pd) => {
+  return new Planet(pd);
+});
 
 // Init scene
 const scene = new THREE.Scene();
 // Init camera
 const camera = buildCamera();
 
-// Update transforms
-sun.mesh.scale.setScalar(5);
-earth.mesh.scale.setScalar(1);
-moon.mesh.scale.setScalar(0.5);
-earth.mesh.position.x = -10;
-moon.mesh.position.x = -15;
-camera.position.z = 10;
+// put planets in their orbit position
+planets.forEach((p) => {
+  p.mesh.position.x = p.orbitRadius;
+});
 
-const axesHelper = new THREE.AxesHelper(100);
+camera.position.z = 10;
 // Add elements to scene
-scene.add(sun.mesh);
-scene.add(earth.mesh);
-scene.add(moon.mesh);
+const axesHelper = new THREE.AxesHelper(100);
+const ambientLight = new THREE.AmbientLight(0xffffff, 5);
+scene.add(ambientLight);
+
+// const pointLight = new THREE.PointLight(0xffffff, 1000);
+// scene.add(pointLight);
+planets.forEach((p) => {
+  scene.add(p.mesh);
+});
 scene.add(axesHelper);
 
 // Init renderer
@@ -61,13 +137,33 @@ function update() {
   const timeDelta = timer.getDelta();
   const elapsedTime = timer.getElapsed();
 
-  const sinTheta = Math.sin(elapsedTime);
-  const cosTheta = Math.cos(elapsedTime);
-  const r = 10;
-  const x = r * cosTheta;
-  const y = r * sinTheta;
+  planets.forEach((p) => {
+    // rotate around orbit
+    const sinTheta = Math.sin(elapsedTime * p.orbitSpeed);
+    const cosTheta = Math.cos(elapsedTime * p.orbitSpeed);
+    const x = p.orbitRadius * cosTheta;
+    const y = p.orbitRadius * sinTheta;
+    p.mesh.position.set(x, 0, y);
+    //self rotation
+    const rotationSpeedAsEulerAngle =
+      THREE.MathUtils.DEG2RAD * p.selfRotationSpeed;
+    const yaw = rotationSpeedAsEulerAngle * timeDelta;
+    p.mesh.rotateY(yaw);
 
-  earth.mesh.position.set(x, 0, y);
+    // apply same processes to moons if any
+    p.moons.forEach((m) => {
+      const sinTheta = Math.sin(elapsedTime * m.orbitSpeed);
+      const cosTheta = Math.cos(elapsedTime * m.orbitSpeed);
+      const x = m.orbitRadius * cosTheta;
+      const y = m.orbitRadius * sinTheta;
+      m.mesh.position.set(x, 0, y);
+      //self rotation
+      const rotationSpeedAsEulerAngle =
+        THREE.MathUtils.DEG2RAD * m.selfRotationSpeed;
+      const yaw = rotationSpeedAsEulerAngle * timeDelta;
+      m.mesh.rotateY(yaw);
+    });
+  });
 
   controls.update();
   renderer.render(scene, camera);
--- src/planet.ts
+++ src/planet.ts
@@ -3,20 +3,39 @@ import * as THREE from "three";
 export interface PlanetData {
   name: string;
   scale: number;
-  color: THREE.ColorRepresentation;
+  orbitRadius: number;
+  orbitSpeed: number;
+  selfRotationSpeed: number;
+  texture: THREE.Texture;
+  moons: PlanetData[];
 }
 
 export class Planet {
   public readonly mesh: THREE.Mesh;
   public readonly name: string;
+  public readonly orbitRadius: number;
+  public readonly orbitSpeed: number;
+  public readonly selfRotationSpeed: number;
+  public readonly moons: Planet[];
 
   constructor(planetData: PlanetData) {
-    const planetMaterial = new THREE.MeshBasicMaterial({
-      color: planetData.color,
+    const planetMaterial = new THREE.MeshStandardMaterial({
+      map: planetData.texture,
     });
     const planetGeometry = new THREE.SphereGeometry(1);
     this.mesh = new THREE.Mesh(planetGeometry, planetMaterial);
     this.mesh.scale.setScalar(planetData.scale);
     this.name = planetData.name;
+    this.orbitRadius = planetData.orbitRadius;
+    this.selfRotationSpeed = planetData.selfRotationSpeed;
+    this.orbitSpeed = planetData.orbitSpeed;
+
+    this.moons = planetData.moons.map((m) => {
+      return new Planet(m);
+    });
+    // make moons child of this planet
+    this.moons.forEach((m) => {
+      this.mesh.add(m.mesh);
+    });
   }
 }
